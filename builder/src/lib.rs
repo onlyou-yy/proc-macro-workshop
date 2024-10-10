cbfacbd19b64f789b39a50feb739953efa579b31
练建发 <958823465@qq.com>
2024-10-10 11:08:42 +0800

    完成builder测试用例2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,98 @@
 use proc_macro::TokenStream;
+use proc_macro2;
+use quote::quote;
+use syn::{self, spanned::Spanned};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
+    // eprintln!("{input_ast:#?}");
 
-    unimplemented!()
+    match do_expand(&st) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
+
+type StructFields = syn::punctuated::Punctuated<syn::Field, syn::Token![,]>;
+
+fn get_fields_from_derive_input(st: &syn::DeriveInput) -> syn::Result<&StructFields> {
+    // 从 DeriveInput 中的 Data 解析出字段名字
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = st.data
+    {
+        return Ok(named);
+    };
+    Err(syn::Error::new_spanned(st, "解析字段名错误"))
+}
+
+fn generate_builder_struct_fields_def(
+    st: &syn::DeriveInput,
+) -> syn::Result<proc_macro2::TokenStream> {
+    let fields = get_fields_from_derive_input(st)?;
+
+    let fields_ident = fields.iter().map(|f| &f.ident);
+    let fields_type = fields.iter().map(|f| &f.ty);
+
+    // 在生成类型的时候要使用绝对路径避免与当前定义的类型冲突
+    // #(重复的内容必须是实现了迭代器的数据)*
+    let ret: proc_macro2::TokenStream = quote! {
+        #(#fields_ident:std::option::Option<#fields_type>),*
+    };
+
+    Ok(ret.into())
+}
+
+fn generate_builder_struct_factory_init_clauses(
+    st: &syn::DeriveInput,
+) -> syn::Result<Vec<proc_macro2::TokenStream>> {
+    // 也可以像 generate_builder_struct_fields_def 一样生成
+
+    let fields = get_fields_from_derive_input(st)?;
+
+    let init_clauses = fields
+        .iter()
+        .map(|f| {
+            let ident = &f.ident;
+            quote! {
+                #ident: std::option::Option::None
+            }
+        })
+        .collect();
+
+    Ok(init_clauses)
+}
+fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    // 获取到结构体的名字 ident;
+    let struct_name_ident = st.ident.clone();
+    // 获取到结构体的名字
+    let struct_name_literal = struct_name_ident.to_string();
+    // 创建 builder 结构体的名字
+    let builder_name = format!("{struct_name_literal}Builder");
+    // 创建 builder 结构体名字的 ident，new的第二个参数是 span，用于定位新增的代码是在哪个位置
+    // 方便之后报错定位错误，这里使用 st.span() ,报错时就会直接提示是修饰的结构的的位置的错误
+    let builder_name_ident = syn::Ident::new(&builder_name, st.span());
+
+    let builder_struct_fields_def = generate_builder_struct_fields_def(st)?;
+    let builder_struct_factory_init_clauses = generate_builder_struct_factory_init_clauses(st)?;
+
+    // 使用 quote! 插入并生成新的 proc_macro2::TokenStream
+    let ret = quote! {
+        pub struct #builder_name_ident {
+            #builder_struct_fields_def
+        }
+
+        impl #struct_name_ident {
+            pub fn builder() -> #builder_name_ident {
+                #builder_name_ident {
+                    #(#builder_struct_factory_init_clauses),*
+                }
+            }
+        }
+    };
+
+    Ok(ret)
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,17 @@
 // To run the code:
 //     $ cargo run
 
+// 将 测试用例的代码复制到这里之后在根目录下运行 cargo expand 可以看到宏生成的代码
+// expand 需要安转，运行 cargo install cargo-expand 安装
+
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
 fn main() {}
